f23c23cd8691e13c1fd42be2edcbd9101b27a417
dulan <madurasinghekldp@gmail.com>
2024-06-27 22:11:51 +0530

    call add employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmployeeComponent } from './components/manage-employee/manage-employee.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmployeeComponent],
+  imports: [RouterOutlet,ManageEmployeeComponent,AddEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css'
 })
 export class AddEmployeeComponent {
+
+  constructor(private http: HttpClient){
+    
+  }
+
+  public employee={
+    firstName:undefined,
+    lastName:undefined,
+    email:undefined,
+    departmentId:undefined,
+    roleId:undefined
+  }
+
+  addEmployee(){
+    console.log(this.employee);
+    this.http.post("http://localhost:8080/emp/add",this.employee).subscribe(data =>{
+      console.log(data);
+    });
+  }
 
 }
