c7333e4d1051d6c2683bfdcb89a16717a9b70593
dulan <madurasinghekldp@gmail.com>
2024-07-04 21:45:55 +0530

    delete confirmation

--- src/app/components/view-employees/view-employees.component.ts
+++ src/app/components/view-employees/view-employees.component.ts
@@ -2,6 +2,7 @@ import { NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-employees',
@@ -21,6 +22,45 @@ export class ViewEmployeesComponent {
   loadEmployeeTable(){
     this.http.get("http://localhost:8080/emp/get-all").subscribe(res=>{
       this.employeeList = res;
+    });
+  }
+
+  deleteEmployee(employee:any){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.delete(`http://localhost:8080/emp/del/${employee.id}`,{responseType:"text"}).subscribe(res=>{
+          this.loadEmployeeTable();
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Employee has been deleted.",
+            icon: "success"
+          });
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Employee has not been deleted.",
+          icon: "error"
+        });
+      }
     });
   }
 
