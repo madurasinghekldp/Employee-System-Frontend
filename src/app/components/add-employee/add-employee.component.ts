6d341a703ff612d1fbc1d6557aca9411c8e71c99
dulan <madurasinghekldp@gmail.com>
2024-07-04 20:31:10 +0530

    sweet alert added

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-employee',
@@ -27,7 +28,12 @@ export class AddEmployeeComponent {
   addEmployee(){
     console.log(this.employee);
     this.http.post("http://localhost:8080/emp/add",this.employee).subscribe(data =>{
-      console.log(data);
+
+      Swal.fire({
+        title: "Success!",
+        text: "New Employee Added!",
+        icon: "success"
+      });
     });
   }
 
