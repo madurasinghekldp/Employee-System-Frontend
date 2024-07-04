cf14348c5469e893880de301953517e88caedfd0
dulan <madurasinghekldp@gmail.com>
2024-07-04 22:27:28 +0530

    add routing to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ManageEmployeeComponent } from './components/manage-employee/manage-employee.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 import { ViewEmployeesComponent } from './components/view-employees/view-employees.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmployeeComponent,AddEmployeeComponent,ViewEmployeesComponent],
+  imports: [RouterOutlet,ManageEmployeeComponent,AddEmployeeComponent,ViewEmployeesComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { ViewEmployeesComponent } from './components/view-employees/view-employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-employee",
+        component: AddEmployeeComponent
+    },
+    {
+        path:"view-employee",
+        component: ViewEmployeesComponent
+    }
+];
