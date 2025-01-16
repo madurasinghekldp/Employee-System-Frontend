a3b45f8b8ae85e182ae673f9546f3799b9ff07c3
dulan <madurasinghekldp@gmail.com>
2025-01-16 23:13:37 +0530

    implement reactive forms for user login and registration with validation

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
+
+  userLoginForm = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('',Validators.required)
+  });
+
+
+  submitLoginForm(){
+
+  }
 
 }
--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CreateUser } from '../../types/createUser';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-registration',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,NgIf],
   templateUrl: './user-registration.component.html',
   styleUrl: './user-registration.component.css'
 })
 export class UserRegistrationComponent {
+
+  userRegForm = new FormGroup({
+    firstName: new FormControl('',Validators.required),
+    lastName: new FormControl('',Validators.required),
+    email: new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('',Validators.required),
+    agreed: new FormControl('',Validators.requiredTrue)
+  });
+
+  public createUser:CreateUser = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+
+  submitRegForm(){
+    this.createUser = {
+      firstName: this.userRegForm.controls.firstName?.value,
+      lastName: this.userRegForm.controls.lastName?.value,
+      email: this.userRegForm.controls.email?.value,
+      password: this.userRegForm.controls.password?.value
+    }
+
+  }
 
 }
