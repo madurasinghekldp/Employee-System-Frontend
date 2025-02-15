8db4d17c0d623ad2e59a8ca96922e007193c1616
dulan <madurasinghekldp@gmail.com>
2025-02-16 00:10:47 +0530

    feat: refactor profile and setting components to use reactive user data from userStore

--- src/app/components/setting/setting.component.ts
+++ src/app/components/setting/setting.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { userStore } from '../../store/user.store';
 
 @Component({
   selector: 'app-setting',
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './setting.component.css'
 })
 export class SettingComponent {
+
+  store = inject(userStore);
+    user = computed(() => this.store.user());
+  
+    firstName:string | undefined;
+    lastName:string | undefined;
+    email:string | undefined;
+    companyName:string | undefined;
+    address:string | undefined;
+    registerNumber:string | undefined;
+  
+    constructor(){
+      effect(()=>{
+        this.firstName = this.user()?.firstName;
+        this.lastName = this.user()?.lastName;
+        this.email = this.user()?.email;
+        this.companyName = this.user()?.company.name;
+        this.address = this.user()?.company.address;
+        this.registerNumber = this.user()?.company.registerNumber;
+      });
+    }
 
 }
