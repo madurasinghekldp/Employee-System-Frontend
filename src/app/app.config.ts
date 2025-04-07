f940193f2f27d1dfd81ccd8856eb0c60bb8b8b9f
dulan <madurasinghekldp@gmail.com>
2025-04-07 19:36:22 +0530

    chore: update Angular dependencies and add Material Design support
    - Added @angular/cdk and @angular/material dependencies to package.json.
    - Updated app.config.ts to include provideAnimationsAsync for better animation handling.
    - Cleaned up layout.component.html by removing unnecessary menu link.
    - Enhanced index.html to include Google Fonts and Material Icons for improved typography and iconography.
    - Updated styles.css to set global styles, including font family and body height.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(),
@@ -11,5 +12,5 @@ export const appConfig: ApplicationConfig = {
         provide: HTTP_INTERCEPTORS,
         useClass: AuthInterceptor,
         multi: true,
-    }]
+    }, provideAnimationsAsync()]
 };
